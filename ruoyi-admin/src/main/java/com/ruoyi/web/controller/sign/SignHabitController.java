bbaf5539c266c3ed4f0afd6a4ae4a43abc851df9
wangcengbing <1396627527@qq.com>
2022-04-29 00:48:16 +0800

    签到打卡模块开发

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/sign/SignHabitController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/sign/SignHabitController.java
@@ -1,6 +1,9 @@
 package com.ruoyi.web.controller.sign;
 
+import java.util.Date;
 import java.util.List;
+
+import com.ruoyi.common.utils.ShiroUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -86,6 +89,9 @@ public class SignHabitController extends BaseController
     @ResponseBody
     public AjaxResult addSave(SignHabit signHabit)
     {
+        // 获取当前用户编号
+        Long userId = ShiroUtils.getUserId();
+        signHabit.setCreateBy(userId+"");
         return toAjax(signHabitService.insertSignHabit(signHabit));
     }
 
